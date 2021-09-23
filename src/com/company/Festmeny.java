594418051ad7fc359fd3ed9b15654e85624b0c8e
Barta Balazs <barta.balazs00@]mail.com>
2021-09-23 11:55:58 +0200

    Main Class áthelyezése

---
+++ src/com/company/Festmeny.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private LocalDateTime legutolsoLicitIdeje;
+    private boolean elkelt;
+
+}
