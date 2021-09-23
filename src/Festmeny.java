82bb9f51333be089da3e238d5963912773c9424a
Barta Balazs <barta.balazs00@]mail.com>
2021-09-23 11:50:01 +0200

    festmeny class elkészítése

---
+++ src/Festmeny.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private LocalDateTime legutolsoLicitIdeje;
+    private boolean elkelt;
+
+}
